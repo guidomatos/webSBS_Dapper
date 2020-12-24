5d7c9df33dc7a37fab8d5a9b80ccfec02d460a4b
guidomatos <guido.matos.88@gmail.com>
2020-12-24 12:51:38 -0500

    Se agregan librerías para Micro-ORM Dapper

---
+++ SBS.Infrastructure/Connection/Dapper/BaseDapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace SBS.Infrastructure.Connection.Dapper
+{
+    public class BaseDapper
+    {
+        protected static IConfiguration _dapperConfiguration;
+
+        public IConfiguration DapperConfiguration
+        {
+            get { return _dapperConfiguration; }
+        }
+
+        public BaseDapper(IConfiguration _configuration)
+        {
+            _dapperConfiguration = _configuration;
+        }
+    }
+}
