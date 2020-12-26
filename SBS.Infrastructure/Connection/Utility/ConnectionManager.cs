3bc07654af8251768664c0ed4dd13e55b4dec492
guidomatos <guido.matos.88@gmail.com>
2020-12-25 20:23:51 -0500

    Se agrega librería y configuración para conexión a Oracle

---
+++ SBS.Infrastructure/Connection/Utility/ConnectionManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using SBS.Infrastructure.Connection.Dapper;
+
+namespace SBS.Infrastructure.Connection.Utility
+{
+    public class ConnectionManager : BaseDapper
+    {
+        public ConnectionManager(IConfiguration _configuration) : base(_configuration)
+        {
+            _dapperConfiguration = _configuration;
+        }
+
+        static internal IDbConnection GetConnection()
+        {
+            string connectionStringName = "ConnectionStrings:ConexionSvr_Default";
+
+            var connectionString = _dapperConfiguration[connectionStringName];
+
+            if (string.IsNullOrEmpty(connectionString))
+                throw new ArgumentException("El parámetro connectionString se encuentra nulo.");
+
+            return new OracleConnection(connectionString);
+        }
+    }
+}
